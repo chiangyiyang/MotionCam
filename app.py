254bfad6e3d9335d4265e95c1c160e582e1807a2
user <user@gmail.com>
2016-09-20 13:05:49 +0800

    Add WebSocket

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from flask import Flask, render_template, Response
+from flask_socketio import SocketIO
 from car4w import *
 
 # emulated camera
@@ -9,28 +10,35 @@ from car4w import *
 from camera_pi import Camera
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!123456'
+socketio = SocketIO(app)
 
 car_t = 0.2
 
+
 @app.route('/go')
 def car_go():
-    Go(car_t*3)
+    Go(car_t * 3)
     return render_template('index.html')
+
 
 @app.route('/back')
 def car_back():
-    Back(car_t*3)
+    Back(car_t * 3)
     return render_template('index.html')
+
 
 @app.route('/turn_r')
 def car_turnR():
     TurnRight(car_t)
     return render_template('index.html')
 
+
 @app.route('/turn_l')
 def car_turnL():
     TurnLeft(car_t)
     return render_template('index.html')
+
 
 @app.route('/')
 def index():
@@ -53,5 +61,20 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
+@socketio.on('car event')
+def handle_car_event(json):
+    print('received car cmd: ' + str(json))
+    # print('cmd: ' + str(json['data']))
+    if (json['data'] == 'go'):
+        Go(car_t * 3)
+    if (json['data'] == 'back'):
+        Back(car_t * 3)
+    if (json['data'] == 'turnl'):
+        TurnLeft(car_t)
+    if (json['data'] == 'turnr'):
+        TurnRight(car_t)
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, threaded=True)
+    # app.run(host='0.0.0.0', debug=True, threaded=True)
+    socketio.run(app=app, host='0.0.0.0')
